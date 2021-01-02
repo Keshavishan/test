d91e49d31f4ef8fc53ae59815507d55aafcbbb11
Keshav Gupta <35744671+Keshavishan@users.noreply.github.com>
2021-01-02 16:11:33 +0000

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5ff09aee7dc2c5001e034522",
+				"5ff09aee7dc2c5001e034524"
+			]
+		},
+		"5ff09aee7dc2c5001e034522": {
+			"id": "5ff09aee7dc2c5001e034522",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5ff09aee7dc2c5001e034524": {
+			"id": "5ff09aee7dc2c5001e034524",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
